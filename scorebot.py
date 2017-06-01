6aa7ea7124ceb074e2a3dd5464e03ce2eed690f4
PietroBiondi <pietro.biondi94@gmail.com>
2017-06-01 15:07:39 +0200

    remove dup service

--- scorebot.py
+++ scorebot.py
@@ -148,7 +148,6 @@ if __name__ == '__main__':
 				'blackgold': Service('blackgold', '20066', blackgold),
 				'sharing': Service('sharing', '20065', sharing),
 				'hanoifones': Service('hanoifones', '20040', hanoifones),
-				'FHMMaintenance': Service('FHMMaintenance', '20111', FHMMaintenance), 
 				'hacker_diary': Service('hacker_diary', '20130', hacker_diary)}
 	
 	'''
